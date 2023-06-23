dbad7a8a2fdb174c8df6fda3088844e9779f7ea7
dmkomur <125804681+dmkomur@users.noreply.github.com>
2023-06-23 15:12:54 +0300

    follow saved on local storage

--- src/CardList/CardList.jsx
+++ src/CardList/CardList.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import logo from "../img/logo.svg";
-
+import { local } from "../services/storage";
 import {
   StyledCard,
   StyledCardButton,
@@ -17,7 +17,10 @@ axios.defaults.baseURL = "https://6492d14f428c3d2035d0be95.mockapi.io/";
 export const CardList = () => {
   const [users, setUsers] = useState([]);
   const [page, setPage] = useState(1);
-
+  const [followList, setFollowList] = useState(() => {
+    const listFromLS = local.load("follow");
+    return listFromLS || [];
+  });
   useEffect(() => {
     const fetchUsers = async () => {
       try {
@@ -34,23 +37,31 @@ export const CardList = () => {
     fetchUsers();
   }, [page]);
 
-  const onFollowBtnClick = (id) => {
+  useEffect(() => {
+    local.save("follow", followList);
+  }, [followList]);
+
+  function updateUser(id, index) {
     const currentIndex = users.findIndex((el) => el.id === id);
     setUsers((prevUsers) => {
       const updatedUsers = [...prevUsers];
       const currentUser = { ...updatedUsers[currentIndex] };
-      if (currentUser.isFollowing) {
-        currentUser.followers -= 1;
-        currentUser.isFollowing = !currentUser.isFollowing;
-        axios.put(`users/${id}`, currentUser);
-      } else {
-        currentUser.followers += 1;
-        currentUser.isFollowing = !currentUser.isFollowing;
-        axios.put(`users/${id}`, currentUser);
-      }
+      currentUser.followers = currentUser.followers + index;
+      axios.put(`users/${id}`, currentUser);
+
       updatedUsers[currentIndex] = currentUser;
       return updatedUsers;
     });
+  }
+  const onFollowBtnClick = (id) => {
+    if (followList.includes(id)) {
+      const newList = followList.filter((item) => item !== id);
+      setFollowList([...newList]);
+      updateUser(id, -1);
+    } else {
+      setFollowList((prev) => [...prev, id]);
+      updateUser(id, 1);
+    }
   };
 
   return (
@@ -62,16 +73,16 @@ export const CardList = () => {
             <StyledAvatar src={user.avatar} alt="avatar" />
           </StyledImgThumb>
           <StyledText style={{ marginBottom: "16px", marginTop: "242px" }}>
-            {user.tweets} Tweets
+            {Intl.NumberFormat().format(user.tweets)} Tweets
           </StyledText>
           <StyledText style={{ marginBottom: "26px" }}>
             {Intl.NumberFormat().format(user.followers)} Followers
           </StyledText>
           <StyledCardButton
-            follow={user.isFollowing}
+            follow={followList.includes(user.id)}
             onClick={() => onFollowBtnClick(user.id)}
           >
-            {user.isFollowing ? "Following" : "Follow"}
+            {followList.includes(user.id) ? "Following" : "Follow"}
           </StyledCardButton>
         </StyledCard>
       ))}
@@ -88,3 +99,20 @@ export const CardList = () => {
     </StyledList>
   );
 };
+
+//  const currentIndex = users.findIndex((el) => el.id === id);
+//  setUsers((prevUsers) => {
+//    const updatedUsers = [...prevUsers];
+//    const currentUser = { ...updatedUsers[currentIndex] };
+//    if (currentUser.isFollowing) {
+//      currentUser.followers -= 1;
+//      currentUser.isFollowing = !currentUser.isFollowing;
+//      axios.put(`users/${id}`, currentUser);
+//    } else {
+//      currentUser.followers += 1;
+//      currentUser.isFollowing = !currentUser.isFollowing;
+//      axios.put(`users/${id}`, currentUser);
+//    }
+//    updatedUsers[currentIndex] = currentUser;
+//    return updatedUsers;
+//  });
